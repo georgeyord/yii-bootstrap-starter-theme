46d158429bd9aa83322b891c9df26a9f0e105fb1
George Yord <george.yord@gmail.com>
2014-03-18 17:35:40 +0200

    Fix typeahead issues in tagsinput

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,7 +125,10 @@ $(function() {
                     options.maxTags = $this.data('maxTags');
 
                 // Set typeahead endpoint and activate typeahead if needed
-                if ($this.data('typeaheadSource') || $this.data('typeaheadLocal') || $this.data('typeaheadRemote')  && typeof options.typeahead != 'object')
+                if ((  $this.data('typeaheadSource')
+                    || $this.data('typeaheadLocal')
+                    || $this.data('typeaheadRemote')
+                    ) && typeof options.typeahead != 'object')
                         options.typeahead = {};
                 if ($this.data('typeaheadSource')) {
                     if (typeof $this.data('typeaheadSource') == 'object')
@@ -137,8 +140,10 @@ $(function() {
                 }
                 if (typeof $this.data('typeaheadLocal') == 'object')
                         options.typeahead.local = $this.data('typeaheadLocal');
-                if (typeof $this.data('typeaheadRemote'))
+                if ($this.data('typeaheadRemote'))
                     options.typeahead.remote = $this.data('typeaheadRemote');
+                if ($this.data('typeaheadTemplate'))
+                    options.typeahead.template = $this.data('typeaheadTemplate');
 
                 if (options.typeahead)
                     options.typeahead.freeInput = ($this.data('typeaheadFreeInput') == true ? true : false);
